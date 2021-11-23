d0ef42b8eeb4b3533b71f1157b17c00ab3c0a48f
Ramsha Jat <68041815+ramshajat@users.noreply.github.com>
2021-11-23 19:37:55 +0500

    Add files via upload
    slab dictionary meaning peace of wood

---
+++ FirstMid/FirstMid/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace FirstMid
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(119, 106);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(38, 15);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "label1";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(468, 55);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(75, 23);
+            this.Calculate.TabIndex = 2;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button Calculate;
+    }
+}
+
---
+++ FirstMid/FirstMid/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstMid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         int salary,count=0;
+        double tax;
+        double t1, t2,t3,t4;
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            salary = Convert.ToInt32(this.textBox1.Text);
+            if(salary>=0 && salary <= 300000)
+            {
+                tax = salary*0.01;
+                this.lbl1.Text = "The tax on first slab is :" +tax +"\nThe total tax is :"+tax;
+            }
+            else if (salary>=300001 && salary <= 500000)
+            {
+                t1 = 300000 *0.01;
+                salary = salary - 300000;
+                t2 = salary *0.03;
+                tax = t1 + t2;
+                lbl1.Text= "The tax on first slab is :" + Convert.ToString(t1) + "\nThe tax on second slab is :" + Convert.ToString(t2) +"\nThe total tax is :" + tax;
+               
+            }
+            else if(salary>=500001 && salary <= 800000)
+            {
+                t1 = 300000 * 0.01;
+                t2 = 200000 * 0.03;
+                salary = salary - 500000;
+                t3 = salary * 0.05;
+                tax = t1 + t2 + t3;
+                lbl1.Text = "The tax on first slab is :" + Convert.ToString(t1) + "\nThe tax on second slab is :" + Convert.ToString(t2) + "\nThe tax on third slab is :" + Convert.ToString(t3) + "\nThe total tax is :" + tax;
+
+            }
+            else if (salary > 800000)
+            {
+                 t1 = 300000 *0.01;
+                 t2 = 200000 *0.03;
+                 t3 = 300000 *0.05;
+                salary = salary - 800000;
+                if (salary >0 &&  salary<200000)
+                {
+                    count += 0;
+                }
+                else
+                {
+                    while (salary >= 200000) {
+                        count = count+1;
+                        salary = salary - 200000;
+                    }
+                }
+                MessageBox.Show("" +count);
+                t4 = count*(200000 *0.02);
+                tax = t1 + t2 + t3 + t4;
+                lbl1.Text = "The tax on first slab is :" + Convert.ToString(t1) + "\nThe tax on second slab is :" + Convert.ToString(t2) + "\nThe tax on third slab is :" + Convert.ToString(t3) + "\nThe tax on fourth slab is :" + Convert.ToString(t4) +"\nThe total tax is :" + tax;
+
+            }
+        }
+    }
+}
